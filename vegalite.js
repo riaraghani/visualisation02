b9ed812f1f27fc0f487ff28cd828c8a728aa1be9
riaraghani <79733413+riaraghani@users.noreply.github.com>
2022-10-13 14:30:01 +1100

    Add files via upload

---
+++ vegalite.js
@@ -0,0 +1,24 @@
+var vg_1 = "QOL_map_year_filter.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance
+    }).catch(console.error);
+
+var vg_2 = "bubble_chart.vg.json";
+vegaEmbed("#bubble", vg_2).then(function(result) {
+    // Access the Vega view instance
+    }).catch(console.error);
+
+var vg_3 = "radial_safety_by_region.json";
+vegaEmbed("#radial", vg_3).then(function(result) {
+    // Access the Vega view instance
+    }).catch(console.error);
+
+var vg_4 = "multiple_area_country_filter.json";
+vegaEmbed("#multiple_area", vg_4).then(function(result) {
+    // Access the Vega view instance
+    }).catch(console.error);
+
+var vg_5 = "unemployment_line.json";
+vegaEmbed("#line", vg_5).then(function(result) {
+    // Access the Vega view instance
+    }).catch(console.error);
